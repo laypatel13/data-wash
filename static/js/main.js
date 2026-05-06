daca0c359fa8cd751d434757a38960fcf49daf4f
laypatel13 <laypatel1322008@gmail.com>
2026-05-07 00:42:53 +0530

    style(ui): progress bars and badge tweaks for analyze page

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,5 @@
-console.log("DataWash loaded");
+// Set progress bar widths from aria-valuenow
+document.querySelectorAll(".progress-bar").forEach(bar => {
+  const val = bar.getAttribute("aria-valuenow");
+  bar.style.width = val + "%";
+});
